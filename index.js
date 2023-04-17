ee8d7d939509ca9a6d6ecf7125d9ab325111e544
Andy Jackson <andypauljackson@gmail.com>
2023-04-17 15:48:45 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,34 @@
+let myEmojis = ["💻", "⛷️", "👨‍🍳"];
+const emojiContainer = document.getElementById("emojiContainer");
+
+function renderEmojis() {
+for (let i = 0; i <myEmojis.length; i++) {
+    const emoji = document.createElement('span');
+    emoji.textContent = myEmojis[i];
+    emojiContainer.append(emoji);
+}
+}
+
+
+const pushBtn = document.getElementById("push-btn");
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input");
+    if (emojiInput.value) { 
+    myEmojis.push(emojiInput.value);
+    emojiInput.value = "";
+    emojiContainer.innerHTML = "";
+    renderEmojis()
+    }  
+})
+
+const unshiftBtn = document.getElementById("unshift-btn");
+unshiftBtn.addEventListener("click", function(){
+    console.log("clicked")
+    const emojiInput = document.getElementById("emoji-input");
+    if (emojiInput.value) { 
+    myEmojis.unshift(emojiInput.value);
+    emojiInput.value = "";
+    emojiContainer.innerHTML = "";
+    renderEmojis()
+    }  
+})
